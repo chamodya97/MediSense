46b6a0eafe144b437a270e8d5b69dfe29c4afacf
chamodya97 <chamodyajayamini22@gmail.com>
2026-09-20 11:04:24 +0530

    Update server port for deployment

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { GoogleGenAI } = require("@google/genai");
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const ai = new GoogleGenAI({
     apiKey: process.env.GEMINI_API_KEY
